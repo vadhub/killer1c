d3fe41ba04d1e0236f4e9f65f0a9e0ea8895d2c5
vadim <gabderahmanov99@gmail.com>
2024-12-15 23:56:11 +0300

    create src file with main file

--- src/main/java/org/example/data/code_gen/Creator.java
+++ src/main/java/org/example/data/code_gen/Creator.java
@@ -9,7 +9,16 @@ import java.io.File;
 import java.io.IOException;
 
 public class Creator {
-    public void createSrc(File code, Analyser analyser, LayoutInflator layoutInflator, RunCode runCode) throws IOException {
+
+    Analyser analyser;
+    LayoutInflator layoutInflator;
+
+    public Creator(Analyser analyser, LayoutInflator layoutInflator) {
+        this.analyser = analyser;
+        this.layoutInflator = layoutInflator;
+    }
+
+    public void createSrc(File code) throws IOException {
         File src = Builder.createSrc(Context.currentProject);
         String newCode = analyser.findTextMain(code, layoutInflator);
         SaveFile.saveFile(src.getAbsolutePath(), "Code.java", newCode);
