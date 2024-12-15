c13d9eea05b7869c425b9d20ad38af72b8adc36f
vadim <gabderahmanov99@gmail.com>
2024-12-16 00:24:21 +0300

    experiment with add text bettwen code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,17 +12,17 @@ import org.example.ui.ProjectTree2;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
         Configurator configurator = new Configurator();
         configurator.createConfig();
         SaveFile saveFile = new SaveFile(Context.currentRootDirectory + File.separator + Context.currentProject);
 
         System.out.println(Context.output());
-
+        replace();
         OpenFile openFile = new OpenFile();
         Local local = new Local();
         Menu menu = new Menu();
@@ -51,6 +51,39 @@ public class Main {
         frameMain.add(codeEditor.createPanel(), BorderLayout.CENTER);
         frameMain.setVisible(true);
 
+    }
+
+    public static void replace() throws IOException {
+        String filePath = Context.currentRootDirectory + File.separator + Context.currentProject + File.separator + "src" + File.separator +"Main.java";
+        String textToAdd = "System.out.println(\"Hello world from code gen\");";
+
+        ArrayList<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Удалить последние 3 строки
+        if (lines.size() >= 3) {
+            lines.remove(lines.size() - 1);
+            lines.remove(lines.size() - 1);
+            lines.remove(lines.size() - 1);
+        }
+
+        // Добавить новые строки
+        lines.add(textToAdd+"}\n}");
+
+        // Записать список строк обратно в файл
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for (String line : lines) {
+                writer.write(line);
+                writer.newLine();
+            }
+        }
     }
 
 
